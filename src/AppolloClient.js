648b7435a11552a00068b42d375cd26b299e950e
osa911 <e.osadchuy@gmail.com>
2020-06-03 21:09:40 +0300

    Update uri for AppolloClient.js

--- src/AppolloClient.js
+++ src/AppolloClient.js
@@ -4,7 +4,7 @@ import { onError } from "apollo-link-error";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
-const uri = process.env.hasOwnProperty('API_SERVER_HOST') ? process.env.API_SERVER_HOST : '/api'
+const uri = process.env.hasOwnProperty('REACT_APP_API') ? process.env.REACT_APP_API : '/api'
 const httpLink = createHttpLink({ uri, credentials: "include" })
 const authLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem('token')

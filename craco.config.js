2e8c4fc2398b16158e43e6c354610f83fca3839a
osa911 <e.osadchuy@gmail.com>
2020-06-16 20:50:09 +0300

    Add charts

--- craco.config.js
+++ craco.config.js
@@ -1,15 +1,21 @@
-const CracoLessPlugin = require('craco-less');
+const CracoLessPlugin = require("craco-less");
+// const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = {
+  // webpack: {
+  //   plugins: [
+  //     new BundleAnalyzerPlugin(),
+  //   ]
+  // },
   plugins: [
     {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
-          modifyVars: { '@primary-color': '#1DA57A' },
-          javascriptEnabled: true,
-        },
-      },
-    },
-  ],
+          modifyVars: { "@primary-color": "#1DA57A" },
+          javascriptEnabled: true
+        }
+      }
+    }
+  ]
 };

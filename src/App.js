f2336215f44727ba0e435e00475934b7d1aa95b3
osa911 <e.osadchuy@gmail.com>
2020-07-01 18:34:48 +0300

    Added russian translation for antd

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react'
+import { ConfigProvider } from 'antd'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import AppolloClient from './AppolloClient'
+import moment from 'moment';
+import 'moment/locale/ru';
+import ruRU from 'antd/es/locale/ru_RU';
 import './styles/App.less'
 import Routes from './routes'
 
+moment.locale('ru');
 const browserHistory = createBrowserHistory()
 
 function App() {
   return (
     <ApolloProvider client={AppolloClient}>
-      <Router history={browserHistory}>
-        <Routes />
-      </Router>
+      <ConfigProvider locale={ruRU}>
+        <Router history={browserHistory}>
+          <Routes />
+        </Router>
+      </ConfigProvider>
     </ApolloProvider>
   )
 }
